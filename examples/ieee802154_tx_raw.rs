5952df25046132ce6e2711cfe026c419d2acaf6d
Paul Otto <potto@ieee.org>
2026-08-09 21:32:11 -0400

    closes 586 with update to payload to decode as data packet

--- examples/ieee802154_tx_raw.rs
+++ examples/ieee802154_tx_raw.rs
@@ -22,13 +22,31 @@ use libtock::runtime::{set_main, stack_size};
 set_main! {main}
 stack_size! {0x600}
 
+const TRANSMIT_INTERVAL_MS: u32 = 1000;
+
+const FCF_BROADCAST_DATA: u16 = 0x9841;
+const BROADCAST_ADDRESS: u16 = 0xffff;
+const MAC_HEADER_LEN: usize = 9;
+
+const BEACON_TAG: &[u8; 7] = b"beacon ";
+const COUNTER_LEN: usize = 2;
+const FRAME_LEN: usize = MAC_HEADER_LEN + BEACON_TAG.len() + COUNTER_LEN;
+const COUNTER_OFFSET: usize = MAC_HEADER_LEN + BEACON_TAG.len();
+
+pub const DEVICE_NAME: &str = "board-a";
+pub const PAN_ID: u16 = 0xcafe;
+pub const SHORT_ADDRESS: u16 = 0x1001;
+pub const LONG_ADDRESS: u64 = 0xdeaddad;
+pub const CHANNEL: u8 = 11;
+pub const TX_POWER: i8 = 4;
+
 fn main() {
     // Configure the radio
-    let pan: u16 = 0xcafe;
-    let addr_short: u16 = 0xdead;
-    let addr_long: u64 = 0xdeaddad;
-    let tx_power: i8 = 4;
-    let channel: u8 = 11;
+    let pan: u16 = PAN_ID;
+    let addr_short: u16 = SHORT_ADDRESS;
+    let addr_long: u64 = LONG_ADDRESS;
+    let tx_power: i8 = TX_POWER;
+    let channel: u8 = CHANNEL;
 
     writeln!(Console::writer(), "Configuring IEEE 802.15.4 radio...\n").unwrap();
 
@@ -66,27 +84,40 @@ fn main() {
     assert!(Ieee802154::is_on());
     writeln!(Console::writer(), "Radio is on!\n").unwrap();
 
-    let mut counter = 0_usize;
-    let mut buf = [
-        b'f', b'r', b'a', b'm', b'e', b' ', b'n', b'.', b'o', b'.', b' ', b'\0', b'\0', b'\0',
-        b'\0',
-    ];
-    fn set_buf_cnt(buf: &mut [u8], counter: &mut usize) {
-        let buf_len = buf.len();
-        let buf_cnt = &mut buf[buf_len - core::mem::size_of_val(&counter)..];
-        buf_cnt.copy_from_slice(&counter.to_be_bytes());
-    }
+    let mut tx_frame = [0_u8; FRAME_LEN];
+    tx_frame[0..2].copy_from_slice(&FCF_BROADCAST_DATA.to_le_bytes());
+    tx_frame[3..5].copy_from_slice(&PAN_ID.to_le_bytes());
+    tx_frame[5..7].copy_from_slice(&BROADCAST_ADDRESS.to_le_bytes());
+    tx_frame[7..9].copy_from_slice(&SHORT_ADDRESS.to_le_bytes());
+    tx_frame[MAC_HEADER_LEN..COUNTER_OFFSET].copy_from_slice(BEACON_TAG);
+
+    let mut sequence = 0_u8;
+    let mut counter = 0_u16;
 
     loop {
-        Alarm::sleep_for(Milliseconds(1000)).unwrap();
+        Alarm::sleep_for(Milliseconds(TRANSMIT_INTERVAL_MS)).unwrap();
 
-        set_buf_cnt(&mut buf, &mut counter);
+        tx_frame[2] = sequence;
+        tx_frame[COUNTER_OFFSET..FRAME_LEN].copy_from_slice(&counter.to_be_bytes());
 
-        // Transmit a frame
-        Ieee802154::transmit_frame_raw(&buf).unwrap();
+        match Ieee802154::transmit_frame_raw(&tx_frame) {
+            Ok(()) => {}
+            Err(error) => {
+                writeln!(Console::writer(), "TX failed: {:?}", error).unwrap();
+                continue;
+            }
+        }
 
-        writeln!(Console::writer(), "Transmitted frame {counter}!\n").unwrap();
+        writeln!(
+            Console::writer(),
+            "TX broadcast: src={:#06x}, sequence={}, count={}",
+            SHORT_ADDRESS,
+            sequence,
+            counter,
+        )
+        .unwrap();
 
-        counter += 1;
+        sequence = sequence.wrapping_add(1);
+        counter = counter.wrapping_add(1);
     }
 }
